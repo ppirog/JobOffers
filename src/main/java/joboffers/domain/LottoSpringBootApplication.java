4292bee991850cc6bc979688ba40c92be02de1d0
ppirog <pawelpirog99@gmail.com>
2024-02-24 16:44:40 +0100

    addded SpringBoot to project

---
+++ src/main/java/joboffers/domain/LottoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package joboffers.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+class LottoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoSpringBootApplication.class, args);
+    }
+}
+
+
