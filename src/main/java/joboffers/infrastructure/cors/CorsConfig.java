579676921b3f71b56d3fedb1395a03e997d37197
ppirog <pawelpirog99@gmail.com>
2024-04-22 00:08:55 +0200

    added CorsConfig

---
+++ src/main/java/joboffers/infrastructure/cors/CorsConfig.java
@@ -0,0 +1,22 @@
+package joboffers.infrastructure.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "http://ec2-18-159-37-230.eu-central-1.compute.amazonaws.com:8000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
